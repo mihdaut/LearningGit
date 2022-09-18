903c316fe331b4333ac66bc80953d15ed56438d8
mihdaut <113821022+mihdaut@users.noreply.github.com>
2022-09-18 09:12:03 +0200

    Update DevClass.java

--- DevClass.java
+++ DevClass.java
@@ -1,7 +1,7 @@
 public class DevClass {
 
     public static void main(String[] args) {
-        System.out.println("dev class");
+        System.out.println("dev class+Forking + PullRequest");
         //kreso did this
         // comment
     }
